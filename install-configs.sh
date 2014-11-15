72ae46d81d6f148e48b1cf858581a2ea1cfad16e
Hector Stalker <1683065+hstalker@users.noreply.github.com>
2014-11-15 23:00:28 +0000

    -Tweaked the installation script to make it simpler

--- install-configs.sh
+++ install-configs.sh
@@ -1,15 +1,7 @@
 #! /usr/bin/sh
 
 cp -rf .i3 /home/hector/.i3
-sudo cp -rf .vim/settings/bundles.vim /home/.vim/bundles.vim
-sudo cp -rf .vim/settings/settings.vim  /home/.vim/settings.vim
-sudo cp -rf .vim/settings/map.vim  /home/.vim/map.vim
-sudo cp -rf .vim/settings/plugins.vim  /home/.vim/plugins.vim
-sudo cp -rf .vim/settings/functions.vim  /home/.vim/functions.vim
-sudo cp -rf .vim/settings/autocmd.vim  /home/.vim/autocmd.vim
-sudo cp -rf .vim/settings/fonts  /home/.vim/fonts
-sudo cp -rf .vim/settings/spell  /home/.vim/spell
-sudo cp -rf .vim/vimrc  /etc/vimrc
+sudo cp -rf .vim /home/.vim
 sudo chmod -R 777 /home/.vim
 cp .bashrc ~/.bashrc
 cp .xinitrc ~/.xinitrc
