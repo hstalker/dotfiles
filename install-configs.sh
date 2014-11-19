c406099a8169788ab9037cb50bd7f1f5d10d44d4
Hector Stalker <1683065+hstalker@users.noreply.github.com>
2014-11-19 22:15:05 +0000

    -Added a .bash_profile for automated i3 initialisation

--- install-configs.sh
+++ install-configs.sh
@@ -2,11 +2,12 @@
 
 sudo pacmatic -Syu pacmatic autoconf automake make gcc ghc cabal-install \
 python2 python3 vim nasm yasm i3 rxvt-unicode xorg-xrdb xorg-xinit dmenu \
-llvm clang boost cmake ttf-dejavu 
+llvm clang boost cmake ttf-dejavu
 cp -rf .i3 ~/
 cp .vimrc ~/.vimrc
 cp -rf .vim ~/
 cp .bashrc ~/.bashrc
+cp .bash_profile ~/.bash_profile
 cp .xinitrc ~/.xinitrc
 cp .Xresources ~/.Xresources
 cp .ycm_extra_conf.py ~/.ycm_extra_conf.py
