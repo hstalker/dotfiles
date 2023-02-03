b039dc37981de24ba6f8e8ceffecd2beff2d0aa3
Hector Stalker <1683065+hstalker@users.noreply.github.com>
2023-02-03 21:15:41 +0000

    Remove `docker-machine` configuration

--- config/docker/.config/shell/modules/login/00-docker.sh
+++ config/docker/.config/shell/modules/login/00-docker.sh
@@ -3,4 +3,3 @@
 # We don't actually provide docker configuration scripts, as they are likely to
 # be usage specific
 silence_output assert_directory "${DOCKER_CONFIG}"
-silence_output assert_directory "${MACHINE_STORAGE_PATH}"
